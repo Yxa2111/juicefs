80e25871eba35d8529d62a4d9e7858da3ddf6004
yxa2111 <yxa2111@qq.com>
2023-07-20 06:35:20 +0800

    add debug log

--- pkg/object/minio.go
+++ pkg/object/minio.go
@@ -58,6 +58,13 @@ func newMinio(endpoint, accessKey, secretKey, token string) (ObjectStorage, erro
 		DisableSSL:       aws.Bool(!ssl),
 		S3ForcePathStyle: aws.Bool(true),
 		HTTPClient:       httpClient,
+		LogLevel:   aws.LogLevel(aws.LogDebugWithEventStreamBody),
+	}
+
+	disable100Continue := strings.EqualFold(uri.Query().Get("disable-100-continue"), "true")
+	logger.Info("minio disable 100 continue", disable100Continue)
+	if disable100Continue {
+		awsConfig.S3Disable100Continue = aws.Bool(true)
 	}
 	if accessKey == "" {
 		accessKey = os.Getenv("MINIO_ACCESS_KEY")
